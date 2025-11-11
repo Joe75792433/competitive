391ab7d55d5c25ff55fadf802c7870cdfe48a5a7
Joe75792433 <67582330+Joe75792433@users.noreply.github.com>
2025-11-11 07:27:05 +0000

    add atcoder environment

---
+++ .devcontainer/atcoder/script/postCreate.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# append to .bashrc
+cat $SRC_FOLDER/bash_append/.bashrc >> ~/.bashrc
+
+# vscode settings files
+rm $WORKSPACE_FOLDER/.vscode/*
+cp $SRC_FOLDER/.vscode/* $WORKSPACE_FOLDER/.vscode/
+
+# online-judge-tools files
+mkdir -p ~/.local/share/online-judge-tools/
+cp $SECRET_FOLDER/atcoder/cookie.jar ~/.local/share/online-judge-tools/cookie.jar
+
+# atcoder-cli files
+mkdir -p ~/.config/atcoder-cli-nodejs/
+cp -r $SRC_FOLDER/atcoder-cli/* ~/.config/atcoder-cli-nodejs/
+cp $SECRET_FOLDER/atcoder/session.json ~/.config/atcoder-cli-nodejs/session.json
+
+# python venv setup
+cd $WORKSPACE_FOLDER
+sudo chown -R vscode:vscode venv
+python3 -m venv venv
+. venv/bin/activate
+pip install -r $SRC_FOLDER/requirements.txt
+deactivate
